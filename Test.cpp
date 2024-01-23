bda702410245bbcae38802b28b93709f827ab4f4
mahoshimada <122493333+mahoshimada@users.noreply.github.com>
2024-01-23 16:12:14 +0900

    Quick update
    This is the test file to see how github desktop works

---
+++ Test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x = 123;
+    string str = "Welcome to CS test file";
+    
+    string name;
+
+    cout << str << endl;
+    cout << "Enter your name: ";
+    cin >> name;
+    
+
+
+    return 0;
+
+}
